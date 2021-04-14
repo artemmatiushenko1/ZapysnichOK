888ccb7b01c78ded600e17a3379598f2ac08a54d
artemmatiushenko1 <artom.matyushenko@gmail.com>
2021-04-14 13:04:27 +0300

    added modal window

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.dir("Hello world!🧠;)");//привет
+console.dir("Hello world!🧠;)"); //привет
+
+//Test modal window
+const addNoteInput = document.querySelector('.add-note-input');
+const overlay = document.querySelector('.overlay');
+const addNoteModal = document.querySelector('.add-note-window');
+const noteHeadlineInput = document.querySelector('.note-headline-input');
+
+addNoteInput.addEventListener('focus', function () {
+    overlay.classList.remove('hidden');
+    addNoteModal.classList.remove('hidden');
+    addNoteInput.blur();
+});
+
+addNoteModal.addEventListener('click', function (e) {
+    e.preventDefault();
+    if (e.target.closest('.btn-close-modal')) {
+        overlay.classList.add('hidden');
+        overlay.classList.add('hidden');
+    }
+})
