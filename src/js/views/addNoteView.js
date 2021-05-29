1dfc17c28a6af81747c7a86a917076b0f36f8a9a
Artem Matiushenko <71723893+artemmatiushenko1@users.noreply.github.com>
2021-05-29 16:10:40 +0300

    Revert "Fixed merge bug"

--- src/js/views/addNoteView.js
+++ src/js/views/addNoteView.js
@@ -1,7 +1,6 @@
 import Modal from './modal.js';
 
 class AddNoteView extends Modal {
-  data;
   rootElement = document.querySelector('.main-container');
   parentElement = document.querySelector('.add-note-form');
   window = document.querySelector('.add-note-window');
@@ -11,7 +10,6 @@ class AddNoteView extends Modal {
   createNoteBtn = document.querySelector('.add-note-btn');
   titleInput = document.querySelector('.note-headline-input');
   descriptionInput = document.querySelector('.note-content-input');
-  selectedFolder = document.querySelector('#folder-select');
 
   constructor() {
     super();
@@ -32,43 +30,11 @@ class AddNoteView extends Modal {
     this.descriptionInput.value = '';
   }
 
-  getSelectedFolder() {
-    const result = this.selectedFolder.value;
-    return result;
-  }
-
   addHandlerAddNote(handler) {
     this.createNoteBtn.addEventListener('click', (e) => {
       e.preventDefault();
       handler();
     });
-  }
-
-  _clear() {
-    this.selectedFolder.innerHTML = '';
-  }
-
-  renderFoldersBar(data) {
-    this.data = data;
-    this._clear();
-    this.selectedFolder.insertAdjacentHTML(
-      'afterbegin',
-      this._generateFoldersBarMarkup()
-    );
-  }
-
-  _generateFoldersBarMarkup() {
-    const foldersBarMarkup = Object.keys(this.data).map(
-      (folderName) => `
-      <option value="${folderName}">${folderName}</option>
-      `
-    );
-    foldersBarMarkup.unshift(
-      `
-      <option value='Всі записи'>Всі записи</option>
-    `
-    );
-    return foldersBarMarkup.join('');
   }
 }
 
